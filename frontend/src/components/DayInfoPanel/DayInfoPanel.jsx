5b45934b4dde6e89ac6988a0818da913b1e25e0b
accepted2 <saleksejgej@gmail.com>
2026-06-06 13:22:13 +0300

    Add i18n translations, fix calendar localization, set Ukrainian as default

--- frontend/src/components/DayInfoPanel/DayInfoPanel.jsx
+++ frontend/src/components/DayInfoPanel/DayInfoPanel.jsx
@@ -65,7 +65,7 @@ const DayInfoPanel = (props) => {
         <div className="day-info-panel__data">
           {displayFeast.icon && (
             <img
-              src={displayFeast.icon}
+              src={`${API_URL}${displayFeast.icon}`}
               className="day-info-panel__feast-image"
               alt={displayFeast.short_title || displayFeast.title}
               loading="lazy"

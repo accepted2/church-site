a2944ae7825b7fd3ac6a88d306c4e732891755b5
accepted2 <saleksejgej@gmail.com>
2026-06-06 12:06:04 +0300

    Fix STORAGES config for media files

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -2,12 +2,16 @@ from django.contrib import admin
 from django.conf import settings
 from django.urls import path, include
 from django.conf.urls.static import static
-from django.conf.urls.i18n import i18n_patterns  # ← добавить импорт
+from django.conf.urls.i18n import i18n_patterns
 
 urlpatterns = [
-                  path('i18n/', include('django.conf.urls.i18n')),  # ← ОБЯЗАТЕЛЬНО добавить
-                  path('admin/', admin.site.urls),
-                  path('api/', include('core.urls')),
-                  path('api/calendar/', include('calendar_app.urls')),
-                  path('api/treby/', include('treby.urls')),
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('i18n/', include('django.conf.urls.i18n')),
+    path('admin/', admin.site.urls),
+    path('api/', include('core.urls')),
+    path('api/calendar/', include('calendar_app.urls')),
+    path('api/treby/', include('treby.urls')),
+]
+
+# 🆕 Раздаём медиа-файлы даже в продакшене
+if not settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
